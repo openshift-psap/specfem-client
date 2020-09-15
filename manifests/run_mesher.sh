f3d6f2992bcf3706f804eec717e26d5d0bcd33ce
Kevin Pouget <kpouget@redhat.com>
2020-09-15 09:46:20 +0200

    resources: add .App.Spec.Resources.RelyOnSharedFS field and use it in run_{mesher,solver}.sh

--- manifests/run_mesher.sh
+++ manifests/run_mesher.sh
@@ -2,13 +2,41 @@
 
 set -e
 
-mkdir /mnt/shared/DATABASES_MPI /mnt/shared/OUTPUT_FILES -p
+if [[ "$SPECFEM_RELY_ON_SHARED_FS" == "true" ]]; then
+    # symlinks from /app/... created in Dockerfile.mesher
+    mkdir -p /mnt/shared/{DATABASES_MPI,OUTPUT_FILES}
+else
+    if [[ "$OMPI_COMM_WORLD_NODE_RANK" -eq 0 ]]; then
+        rm -rf /app/{DATABASES_MPI,OUTPUT_FILES} # remove the symlinks
+        mkdir /app/{DATABASES_MPI,OUTPUT_FILES} -p
+    fi
 
-cd app 
+    if [[ -z "$OMPI_COMM_WORLD_RANK" || "$OMPI_COMM_WORLD_RANK" -eq 0 ]]; then
+        rm -rf /mnt/shared/DATABASES_MPI/
+        mkdir /mnt/shared/DATABASES_MPI
+    fi
+fi
 
-./bin/xmeshfem3D
+cd /app && ./bin/xmeshfem3D
 
-if [[ -z "$OMPI_COMM_WORLD_RANK" || $OMPI_COMM_WORLD_RANK -eq 0 ]]; then
-  cat OUTPUT_FILES/output_mesher.txt | grep "buffer creation in seconds"
+if [[ -z "$OMPI_COMM_WORLD_RANK" || "$OMPI_COMM_WORLD_RANK" -eq 0 ]]; then
+  cat /app/OUTPUT_FILES/output_mesher.txt | grep "buffer creation in hh:mm:ss"
   env > /mnt/shared/env.mesher
 fi
+
+[[ "$SPECFEM_RELY_ON_SHARED_FS" == "true" ]] && exit 0
+
+if [[ -z "$OMPI_COMM_WORLD_RANK" || $OMPI_COMM_WORLD_RANK -eq 0 ]]; then
+    cp /app/OUTPUT_FILES/ /mnt/shared/ -r
+fi
+
+if [[ -z "$OMPI_COMM_WORLD_RANK" ]]; then
+    cp /app/DATABASES_MPI/* /mnt/shared/DATABASES_MPI/
+else
+    if [[ "$OMPI_COMM_WORLD_NODE_RANK" -eq 0 ]]; then
+        cp /app/DATABASES_MPI/* /mnt/shared/DATABASES_MPI/
+    fi
+fi
+
+echo Mesher done $OMPI_COMM_WORLD_RANK
+
--- manifests/run_solver.sh
+++ manifests/run_solver.sh
@@ -2,12 +2,25 @@
 
 set -e
 
-cd app
+if [[ "$SPECFEM_RELY_ON_SHARED_FS" != "true" ]]; then
+    if [[ "$OMPI_COMM_WORLD_NODE_RANK" -eq 0 ]]; then
+        rm -f /app/{DATABASES_MPI,OUTPUT_FILES} # remove the symlinks
+        cp /mnt/shared/{DATABASES_MPI,OUTPUT_FILES} /app/ -r
+    fi
+fi
 
-./bin/xspecfem3D
+cd /app && ./bin/xspecfem3D
 
 if [[ -z "$OMPI_COMM_WORLD_RANK" || $OMPI_COMM_WORLD_RANK -eq 0 ]]; then
-  cp oc.build.log /mnt/shared/
+  cp /app/oc.build.log /mnt/shared/
   env > /mnt/shared/env.solver
-  cat OUTPUT_FILES/output_solver.txt | grep "Total elapsed time in seconds"
+  cat /app/OUTPUT_FILES/output_solver.txt | grep "Total elapsed time in hh:mm:ss"
 fi
+
+[[ "$SPECFEM_RELY_ON_SHARED_FS" == "true" ]] && exit 0
+
+if [[ -z "$OMPI_COMM_WORLD_RANK" || $OMPI_COMM_WORLD_RANK -eq 0 ]]; then
+    cp /app/OUTPUT_FILES/* /mnt/shared/OUTPUT_FILES/
+fi
+
+echo Solver done $OMPI_COMM_WORLD_RANK
